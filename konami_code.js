781d33c2e28f9201b2f7472f246b8996fc5f161e
Parker Preyer <parker.preyer@gmail.com>
2019-11-08 22:21:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,22 +13,24 @@ const codes = [
 
 let index = 0;
 
-function init() {
-  function onKeyDownHandler(e) {
-    const key = e.key;
+function onKeyDownHandler(e) {
+  const key = e.key;
 
-    if (key === codes[index]) {
-      index++;
+  if (key === codes[index]) {
+    index++;
 
-      if (index === codes.length) {
-        alert('Congrats!!!');
-
-        index = 0;
-      }
-    } else {
-      alert('Keep trying ;(');
+    if (index === codes.length) {
+      alert('Congrats!!!');
 
       index = 0;
-    }      
+    }
+  } else {
+    alert('Keep trying ;(');
+
+    index = 0;
   }
+}
+
+function init() {
+  onKeyDownHandler(e);
 }
